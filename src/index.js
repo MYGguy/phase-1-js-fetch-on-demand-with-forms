652860e729887cbcd6a140fb0187abe6c4877267
Moshe Goldstein <mygguy613@gmail.com>
2025-01-19 16:24:57 -0500

    remove () from (res) and (data)

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const init = () => {
         console.log(input.value);
 
         fetch(`http://localhost:3000/movies/${input.value}`)
-            .then((res) => res.json())
-            .then((data) => {
+            .then(res => res.json())
+            .then(data => {
                 console.log(data);
                 const title = document.querySelector('section#movieDetails h4');
                 const summary = document.querySelector('section#movieDetails p');
